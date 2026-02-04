132512b1d7ce8ae71addd906ff4580fb1143da8b
Amalraj MP <amalrajmp72@gmail.com>
2026-02-04 19:01:54 +0000

    feat(cart): add backend remove from cart api

--- backend/routes/cartRoutes.js
+++ backend/routes/cartRoutes.js
@@ -6,6 +6,9 @@ router.post('/add', async (req, res) => {
   const { userId, productId } = req.body
 
   const user = await User.findById(userId)
+  if (!user) {
+    return res.status(404).json({ message: 'User not found' })
+  }
 
   const item = user.cartList.find(
     (item) => item.productId.toString() === productId,
@@ -21,4 +24,19 @@ router.post('/add', async (req, res) => {
   res.json(user.cartList)
 })
 
+router.post('/remove', async (req, res) => {
+  const { userId, productId } = req.body
+
+  const user = await User.findById(userId)
+  if (!user) {
+    return res.status(404).json({ message: 'User not found' })
+  }
+
+  user.cartList = user.cartList.filter(
+    (item) => item.productId.toString() !== productId,
+  )
+
+  await user.save()
+  res.json(user.cartList)
+})
 module.exports = router
