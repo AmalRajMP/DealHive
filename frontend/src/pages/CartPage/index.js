0fd2b2da555fa78187b801893ee00e8d0f428111
Amalraj MP <amalrajmp72@gmail.com>
2026-02-11 19:59:37 +0000

    style(cart): enhance responsive empty and failure state visuals

--- frontend/src/pages/CartPage/index.js
+++ frontend/src/pages/CartPage/index.js
@@ -1,39 +1,104 @@
-import { useEffect, useContext } from 'react'
+import { useEffect, useContext, useState } from 'react'
 import CartContext from '../../context/CartContext'
 
 import CartItem from '../../components/CartItem'
-
 import Navbar from '../../components/Navbar'
+
+import { ThreeDots } from 'react-loader-spinner'
+import { MdErrorOutline } from 'react-icons/md'
+
+import apiStatusConstants from '../../constants/apiStatusConstants'
 
 import {
   CartContainer,
   CartHeading,
   CartList,
   EmptyView,
+  EmptyImage,
+  LoaderContainer,
+  FailureContainer,
+  FailureImage,
+  FailureText,
+  RetryButton,
 } from './styledComponents'
 
 const CartPage = () => {
   const { cartList, fetchCart } = useContext(CartContext)
+  const [apiStatus, setApiStatus] = useState(apiStatusConstants.initial)
+
+  const getCart = async () => {
+    try {
+      setApiStatus(apiStatusConstants.inProgress)
+      await fetchCart()
+      setApiStatus(apiStatusConstants.success)
+    } catch (error) {
+      setApiStatus(apiStatusConstants.failure)
+    }
+  }
 
   useEffect(() => {
-    fetchCart()
-  }, [fetchCart])
+    getCart()
+  }, [])
+
+  const renderLoadingView = () => (
+    <LoaderContainer>
+      <ThreeDots color="#1e40af" height="50" width="50" radius="9" />
+    </LoaderContainer>
+  )
+
+  const renderFailureView = () => (
+    <FailureContainer>
+      <FailureImage
+        src="https://assets.ccbp.in/frontend/react-js/nxt-trendz-error-view-img.png"
+        alt="failure view"
+      />
+      <FailureText>Failed to load cart items</FailureText>
+      <RetryButton onClick={getCart}>Retry</RetryButton>
+    </FailureContainer>
+  )
+
+  const renderSuccessView = () => {
+    if (cartList.length === 0) {
+      return (
+        <EmptyView>
+          <EmptyImage
+            src="https://assets.ccbp.in/frontend/react-js/nxt-trendz-empty-cart-img.png"
+            alt="empty cart"
+          />
+          <p>Your cart is empty</p>
+        </EmptyView>
+      )
+    }
+
+    return (
+      <CartContainer>
+        <CartHeading>My Cart</CartHeading>
+        <CartList>
+          {cartList.map((item) => (
+            <CartItem key={item.productId._id} item={item} />
+          ))}
+        </CartList>
+      </CartContainer>
+    )
+  }
+
+  const renderCartPage = () => {
+    switch (apiStatus) {
+      case apiStatusConstants.inProgress:
+        return renderLoadingView()
+      case apiStatusConstants.success:
+        return renderSuccessView()
+      case apiStatusConstants.failure:
+        return renderFailureView()
+      default:
+        return null
+    }
+  }
 
   return (
     <>
       <Navbar />
-      {cartList.length === 0 ? (
-        <EmptyView>Your cart is empty</EmptyView>
-      ) : (
-        <CartContainer>
-          <CartHeading>My Cart</CartHeading>
-          <CartList>
-            {cartList.map((item) => (
-              <CartItem key={item.productId._id} item={item} />
-            ))}
-          </CartList>
-        </CartContainer>
-      )}
+      {renderCartPage()}
     </>
   )
 }
