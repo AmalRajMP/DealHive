79128253c24ad3545b13d42ed9b0ffe93b995215
Amalraj MP <amalrajmp72@gmail.com>
2026-02-15 14:49:02 +0000

    feat(auth): add role field for admin authorization

--- backend/models/User.js
+++ backend/models/User.js
@@ -1,6 +1,11 @@
 const mongoose = require('mongoose')
 
 const userSchema = new mongoose.Schema({
+  role: {
+    type: String,
+    enum: ['user', 'admin'],
+    default: 'user',
+  },
   firstName: {
     type: String,
     required: true,
