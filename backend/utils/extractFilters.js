eecec4c883de5722757e6852dc4a1048b5e6afc2
Amal Raj M P <amalrajmp72@gmail.com>
2026-06-11 11:28:19 +0530

    fix(filters): improve filter extraction logic

--- backend/utils/extractFilters.js
+++ backend/utils/extractFilters.js
@@ -2,13 +2,39 @@ const { filterCategories } = require("../constants/filterCategories")
 
 const extractFilters = (userQuery) => {
   const lower = userQuery.toLowerCase()
+
+  let minPrice = null
+  let maxPrice = null
+
+  const maxPriceIndicators = [
+    "under",
+    "below",
+    "less than",
+    "upto",
+    "up to",
+    "within",
+  ]
+
+  const minPriceIndicators = [
+    "above",
+    "over",
+    "greater than",
+    "more than",
+    "starting from",
+  ]
+
+  const rangeKeywords = ["between"]
+
   const matchingCategories = filterCategories.filter(
     (eachCategory) =>
       lower.includes(eachCategory.name.toLowerCase()) ||
       eachCategory.aliases?.some((alias) => lower.includes(alias)),
   )
 
-  return matchingCategories
+  return {
+    categories: matchingCategories,
+    maxPrice: null,
+  }
 }
 
 module.exports = { extractFilters }
